083c78f9817c3a0b79c66f9547acef4d690e389b
MYYOOMI <93776430+mrmyyoochannel@users.noreply.github.com>
2025-02-03 00:12:54 +0700

    Update nav.php

--- _Php_Script/nav.php
+++ _Php_Script/nav.php
@@ -12,7 +12,7 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../php_webapp_PJ02/">หน้าหลัก</a>
+            <a class="nav-link active" aria-current="page" href="..">หน้าหลัก</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="https://myyoomi.carrd.co/">Create By MYYOOMI</a>
